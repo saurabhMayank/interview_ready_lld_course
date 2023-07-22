aa18d64e92a0fed2e119dc12cb9f29e94c74fd45
saurabhMayank <82028762+saurabhMayank@users.noreply.github.com>
2023-07-22 09:54:11 +0000

    added code to make doubly link lisst

--- cache_lld/storage.py
+++ cache_lld/storage.py
@@ -33,3 +33,54 @@ class Storage:
         }else {
             return false
         }
+
+# not implementing all the methods
+# in LLD coding -> only the structure and imp methods -> are necessary
+# other simple methods are not necessary to be implement
+# simply put the method definition -> and call the method
+
+class DLL:
+
+    def __init__(self):
+        self.dll_node = DLLNode()
+    
+    def detach_node(self, node: DLLNode, pos):
+        pass
+
+    def attach_node_at_tail(self, node: DLLNode):
+        pass
+
+
+
+class DLLNode:
+
+    def __init__(self):
+        pass
+
+from abc import ABC, abstractmethod
+
+# abstract class of eviction policy
+# this class extended -> to create various eviction policies 
+class IEvictionPolicy(ABC):
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def key_accessed(self, key: str):
+        pass
+
+    @abstractmethod
+    def evict_key(self):
+        pass
+
+
+class LRUEvictionPolicy(IEvictionPolicy):
+    def __init__(self):
+        self.dll = DLL()
+        self.dll_node = DLLNode()
+        # {key: DLLNode}
+        # address mapper of each node
+        # DLLNode object contains address of the node
+        self.pos_mapper = {}
+
+
